6172d4aac95a5a121e0c736a0af007fb63a12879
Laza-adina <lazaher23@gmail.com>
2026-03-21 10:33:06 +0300

    cors prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'https://tsenabe.app',
+  'https://www.tsenabe.app',
+    
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
